dcf5f367ce57cdb992e85f47699f8b4adba5819c
mohitfw24 <mohitsingh2443@gmail.com>
2022-12-05 23:18:27 +0530

    logins

---
+++ login.js
@@ -0,0 +1,21 @@
+Unit -1 
+
+Sprint - 3
+
+Day -4 
+
+Assignment ---
+
+About of Masai school..
+
+
+MASAI SCHOOL is the best way of skill and Develop your career in Tech. this platform also gives a lots of knowledge . Many Students takes admission got a soft skills and they have improve your English Communication . Many Instructor solving the doubts of students. so that's a knowledge of Hub for Prgramming Language.
+
+
+My Journey at  masai school...
+
+First of all i will take the admission to give Msat test. After some days i got a my result . so i have take a class on Monday. afterthat , i known about my intructor and all curriculum .
+so that, I have take the first class of Coding as Java Script that's my favourite programming language. from 1 week i faced curcumstances in Coding because that was my first expeienced. 
+so i have faced with various problem. but i am not give up.  
+   
+  
